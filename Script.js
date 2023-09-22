eb5c546226632b3b782c149842d883a34b7d561e
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-22 18:30:11 +0530

    colochanger

---
+++ Script.js
@@ -0,0 +1,32 @@
+const randomColor = function() {
+    const hex = '0123456789ABCDEF';
+    let color = "#"; //hex srart wth #
+    for(let i=0; i< 6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+let IntervalId;
+
+const startChangingColor = function(){
+if(!IntervalId){
+   IntervalId= setInterval(changeBgColor,1000);
+}
+
+
+    function changeBgColor() {
+   document.body.style.backgroundColor = randomColor();
+    }
+};
+
+const stopChangingColor = function() { 
+    clearInterval(IntervalId);
+    //IntervalId = null; //to make your code more enhanced
+};
+
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click',stopChangingColor);
+
